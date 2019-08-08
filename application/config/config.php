624610e75a52fbe3732d8254720262186a11fb45
masjum <jumarikom@gmail.com>
2019-08-09 00:47:39 +0700

    perbaikan index

--- application/config/config.php
+++ application/config/config.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-$config['base_url'] = 'https://pemdesgrenden.com/';
+$config['base_url'] = 'http://localhost/';
 
 /*
 |--------------------------------------------------------------------------
