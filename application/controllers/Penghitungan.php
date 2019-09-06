957f73e20df477a43ccccc7ecbf61d7f7f6b1a91
masjum <jumarikom@gmail.com>
2019-09-06 11:09:37 +0700

    Perbaikan hasil hitung

--- application/controllers/Penghitungan.php
+++ application/controllers/Penghitungan.php
@@ -9,26 +9,26 @@ class Penghitungan extends CI_Controller {
 		$this->load->view('penghitungan/Index_html', $data);
    }
 
-   public function get_result(Type $var = null)
+public function get_result(Type $var = null)
    {
-      $tot_global       = $this->Query->select_where_group_by('pilkades_hitung_manual', 
-                                    array('COUNT(id) as jml_suara'),
+      $tot_global       = $this->Query->select_where_join2_group_by('pilkades_hitung_manual', 'pilkades_calon', 'pilkades_calon.id=pilkades_hitung_manual.id_cal',
+                                    array('COUNT(pilkades_hitung_manual.id) as jml_suara'),
                                     array(),
-                                    array(),0,1, 'id ASC')->row();
-      $per_cal       = $this->Query->select_where_group_by('pilkades_hitung_manual', 
-                                    array('id_cal', 'COUNT(id) as jml_suara'),
-                                    array('id_cal'),
+                                    array(),0,1, 'pilkades_hitung_manual.id ASC')->row();
+      $per_cal       = $this->Query->select_where_join2_group_by('pilkades_hitung_manual', 'pilkades_calon', 'pilkades_calon.id=pilkades_hitung_manual.id_cal',
+                                    array('pilkades_hitung_manual.id_cal', 'COUNT(pilkades_hitung_manual.id) as jml_suara'),
+                                    array('pilkades_hitung_manual.id_cal'),
                                     array(),0,25, 'id_cal ASC')->result_array();
-      $get_suara  = $this->Query->select_where_group_by('pilkades_hitung_manual', 
-                                    array('id_dapil', 'COUNT(id) as jml_suara'),
-                                    array('id_dapil'),
-                                    array(),0,25, 'id ASC')->result_array();
+      $get_suara  = $this->Query->select_where_join2_group_by('pilkades_hitung_manual', 'pilkades_calon', 'pilkades_calon.id=pilkades_hitung_manual.id_cal',
+                                    array('pilkades_hitung_manual.id_dapil', 'COUNT(pilkades_hitung_manual.id) as jml_suara'),
+                                    array('pilkades_hitung_manual.id_dapil'),
+                                    array(),0,25, 'pilkades_hitung_manual.id ASC')->result_array();
       foreach ($get_suara as $key => $value) {
         $dapil[$value['id_dapil']]    = $value['jml_suara'];
-      }
-      $result        = $this->Query->select_where_group_by('pilkades_hitung_manual', 
-                                          array('id_cal', 'id_dapil', 'COUNT(id) as jml_suara'),
-                                          array('id_cal', 'id_dapil'),
+      } 
+      $result        = $this->Query->select_where_join2_group_by('pilkades_hitung_manual', 'pilkades_calon', 'pilkades_calon.id=pilkades_hitung_manual.id_cal',
+                                          array('pilkades_hitung_manual.id_cal', 'pilkades_hitung_manual.id_dapil', 'COUNT(pilkades_hitung_manual.id) as jml_suara'),
+                                          array('pilkades_hitung_manual.id_cal', 'pilkades_hitung_manual.id_dapil'),
                                           array(),0,25, 'id_cal, id_dapil ASC')->result_array();
       echo json_encode(array('tot_global'=> $tot_global->jml_suara, 'dapil'=> $dapil, 'result'=> $result, 'per_cal'=> $per_cal));
    }
